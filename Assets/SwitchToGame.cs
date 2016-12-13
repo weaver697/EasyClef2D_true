914e63ca0bcc65d14e051ec59cccb47e189136f3
weaver697 <weaver697@wp.pl>
2016-12-13 19:32:46 +0100

    menu
    menu

---
+++ Assets/SwitchToGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SwitchToGame : MonoBehaviour {
+
+	public int scene;
+
+	void Start () {
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("Launching scene " + scene);
+		SceneManager.LoadScene ("game_" + scene);
+	}
+}
